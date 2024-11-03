fa75957aebde9ffaf7ee3cd43542af505b493636
mlekhi <maya.lekhi1@gmail.com>
2024-11-03 17:02:58 -0500

    hiding ugly menu

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@
  */
 
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
@@ -16,9 +17,10 @@ const Stack = createNativeStackNavigator();
 const App: React.FC = () => {
   return (
     <NavigationContainer>
+      <StatusBar hidden={true} />
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}  />
-        <Stack.Screen name="Game" component={GameScreen} options={{ headerShown: true }}  />
+        <Stack.Screen name="Game" component={GameScreen} options={{ headerShown: false }}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
